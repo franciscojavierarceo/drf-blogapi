75706a16de3e250d9f299ecb9d7e4901c0219407
rounak21 <babisantos34@gmail.com>
2019-12-24 11:53:16 +0530

    testing_api

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -152,9 +152,9 @@ USE_TZ = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'roughosh47@gmail.com'
-EMAIL_HOST_PASSWORD = 'babi21091992#'
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT'))
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 
 AUTH_USER_MODEL = 'users.CustomUser'
 # AUTH_USER_MODEL = 'auth.User'
