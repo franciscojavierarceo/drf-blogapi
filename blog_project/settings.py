318219933871ecec9f1a9b34ae5b07f3c829b27e
Francisco Javier Arceo <arceofrancisco@gmail.com>
2020-01-01 21:02:45 -0500

    updated settings to remove secret key

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 ENVIRONMENT = os.environ.get('ENVIRONMENT', default='development')
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mo&431a+djcw_yc_sjy2a08l9qg&#nylxh9s2$rtgp!a4wsnyh'
+SECRET_KEY = os.environ.get('SECRET_KEY') 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
